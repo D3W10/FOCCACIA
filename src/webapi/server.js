903b6d278df85351defbeac2cd1f6d02df1ca8d5
jonazzzzzzzz <joaoncpires2004@gmail.com>
2024-11-04 11:24:57 +0000

    feat: add api endpoints

--- src/webapi/server.js
+++ src/webapi/server.js
@@ -5,7 +5,12 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/teams", webApi.searchClubByName)
-
+app.post("/groups", webApi.createGroup)
+app.put("/groups/:id", webApi.editGroup)
+app.get("/groups", webApi.listGroup)
+app.delete("/groups/:id", webApi.deleteGroup)
+app.get("/groups/:id", webApi.getDetailsOfGroup)
+app.post("/groups/:id/teams", webApi.addTeamToGroup)
+app.delete("/groups/:id/teams/:idt", webApi.removeTeamFromGroup)
 
 app.listen(8080, ()=>console.log("Listening..."))
