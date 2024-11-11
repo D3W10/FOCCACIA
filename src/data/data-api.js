d4c30a3323b78b7e42ad224d6f05c9068a3d4d88
Daniel Nunes <danyribeironunes15@gmail.com>
2024-11-11 11:27:01 +0000

    feat: Add error management module and first two external API calls

--- src/data/data-api.js
+++ src/data/data-api.js
@@ -1,38 +1,83 @@
+import { getError } from "../utils/errorManager";
+
 const API_URL = "https://v3.football.api-sports.io";
 const API_KEY = "83a825990145b3950d8add20554c52e6";
 
-async function searchClubByName(name) {
-    const response = await fetch(`${API_URL}?id=${id}`, {
-        method: 'GET',
-        headers: {
-            'x-apisports-key': API_KEY // Inclui a chave da API no cabeçalho da requisição
-        }
-    });
-    const data = await response.json(); // Converte a resposta para formato JSON
-
-    // Verifica se a resposta contém dados válidos
-    if (data.response && data.response.length > 0) {
-        return {
-            id: data.response[0].team.id,
-            name: data.response[0].team.name,
-            stadium: data.response[0].venue.name
-        };
-    } else {
-        console.log(`Nenhum dado encontrado para a equipa com id ${id}`);
-        return null; // Retorna null se não houver dados
-    }
-
+/**
+ * 
+ * @param {String} name 
+ * @throws {TypeError}
+ */
+export async function getTeamsByName(name) {
+    try {
+        const response = await fetch(`${API_URL}/teams?name=${name}`, {
+            method: "GET",
+            headers: {
+                "x-apisports-key": API_KEY
+            }
+        });
     
-
-
-
-
-
-
+        if (response.ok) {
+            const data = await response.json();
+    
+            if (data.response && data.response.length > 0)
+                return data.response.map(t => ({
+                    id: t.team.id,
+                    name: t.team.name,
+                    country: t.team.country,
+                    founded: t.team.founded,
+                    logo: t.team.logo,
+                    stadium: t.venue.name,
+                    city: t.venue.city
+                }));
+            else
+                return [];
+        }
+        else
+            throw new Error(getError("EXTERNAL_ERROR"));
+    }
+    catch (error) {
+        if (error instanceof SyntaxError)
+            throw new Error(getError("JSON_PARSE_ERROR"));
+        else
+            throw new Error(getError("UNKNOWN_ERROR"));
+    }
 }
 
-export const data = {
-    searchClubByName
-}
+/**
+ * 
+ * @param {Number} id 
+ * @throws {TypeError}
+ */
+export async function getLeaguesByTeam(id) {
+    try {
+        const response = await fetch(`${API_URL}/leagues?team=${id}`, {
+            method: "GET",
+            headers: {
+                "x-apisports-key": API_KEY
+            }
+        });
 
-export default data
+        if (response.ok) {
+            const data = await response.json();
+            
+            if (data.response && data.response.length > 0)
+                return data.response.map(l => ({
+                    id: l.league.id,
+                    name: l.league.name,
+                    logo: l.league.logo,
+                    country: l.country.name
+                }));
+            else 
+                return [];
+        }
+        else
+            throw new Error(getError("EXTERNAL_ERROR"));
+    }
+    catch (error) {
+        if (error instanceof SyntaxError)
+            throw new Error(getError("JSON_PARSE_ERROR"));
+        else
+            throw new Error(getError("UNKNOWN_ERROR"));
+    }
+}
---
+++ src/utils/errorManager.js
@@ -0,0 +1,55 @@
+/**
+ * @typedef {keyof typeof errorCodes} ErrorCode
+ * 
+ * @typedef {Object} ErrorObj
+ * @property {Number} code
+ * @property {String} message
+ */
+
+const errorCodes = {
+    EXTERNAL_ERROR: -3,
+    JSON_PARSE_ERROR: -2,
+    UNKNOWN_ERROR: -1
+};
+
+/**
+ * @type {Record<ErrorCode, string>}
+ */
+const errorMessages = {
+    EXTERNAL_ERROR: "An external error occurred",
+    JSON_PARSE_ERROR: "Error parsing JSON response",
+    UNKNOWN_ERROR: "An unknown error occurred on our side"
+};
+
+/**
+ * 
+ * @param {keyof typeof errorCodes} code 
+ */
+export function getError(code) {
+    return `${errorCodes[code]} | ${errorMessages[code]}`;
+}
+
+/**
+ * 
+ * @param {ErrorCode} error 
+ * @param {Object} data
+ * @param {Number} status
+ */
+export function buildHTTPError(error, data, status = 200) {
+    const errorCode = errorCodes[error];
+    if (errorCode == 0)
+        return [{
+            code: 0,
+            data
+        }, status];
+    else if (errorCode > 0)
+        return [{
+            code: errorCode,
+            message: errorMessages[error]
+        }, 400];
+    else
+        return [{
+            code: -1,
+            message: errorMessages[-1]
+        }, 500];
+}
