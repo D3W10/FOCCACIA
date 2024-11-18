bcef7ff5fa1989a23573bc984bf25d48cb25dc77
Daniel Nunes <danyribeironunes15@gmail.com>
2024-11-18 10:37:36 +0000

    style: Add semicolons, spaces and remove comments from server.js

--- src/webapi/server.js
+++ src/webapi/server.js
@@ -1,25 +1,17 @@
-import express from 'express'
-import webApi from './web-api.js'
+import express from "express"
+import webApi from "./web-api.js"
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-/*
-put update
-get obter
-post criar
-delete apagar
-*/
-//app.get("/teams", webApi.searchClubByName)
+app.post("/groups", webApi.createGroup);
+app.put("/groups/:id", webApi.editGroup);
+app.get("/groups", webApi.listGroup);
+app.delete("/groups/:id", webApi.deleteGroup);
+app.get("/groups/:id", webApi.getDetailsOfGroup);
+app.post("/groups/:id/teams", webApi.addTeamToGroup);
+app.delete("/groups/:id/teams/:idt", webApi.removeTeamFromGroup);
+app.post("/user", webApi.createUser);
 
-app.post("/groups", webApi.createGroup)
-app.put("/groups/:id", webApi.editGroup)
-app.get("/groups", webApi.listGroup)
-app.delete("/groups/:id", webApi.deleteGroup)
-app.get("/groups/:id", webApi.getDetailsOfGroup)
-app.post("/groups/:id/teams", webApi.addTeamToGroup)
-app.delete("/groups/:id/teams/:idt", webApi.removeTeamFromGroup)
-app.post("/user", webApi.createUser)
-
-app.listen(8080, ()=>console.log("Listening..."))
+app.listen(8080, () => console.log("Listening..."));
