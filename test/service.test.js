9469028ab647f260f3698bb03a3f7524c309dd8d
Daniel Nunes <danyribeironunes15@gmail.com>
2024-11-30 10:30:19 +0000

    test: Add a fake API module for testing purposes and improve existing test cases

--- test/service.test.js
+++ test/service.test.js
@@ -2,7 +2,7 @@ import { beforeEach, describe, it } from "mocha";
 import { expect, use} from "chai";
 import chaiAsPromised from "chai-as-promised";
 import serviceBuilder from "../src/service/service.js";
-import api from "../src/data/data-api.js";
+import api from "../src/data/data-api-fake.js";
 import foccacia from "../src/data/data-foccacia.js";
 
 use(chaiAsPromised);
@@ -20,57 +20,59 @@ describe("Service", () => {
         });
 
         it("Should return empty for non-existing team", async () => {
-            expect(await service.searchTeams("3")).to.deep.equal([]);
+            expect(await service.searchTeams("0")).to.deep.equal([]);
         });
     });
 
     describe("searchLeagues()", () => {
-        it("Should return all teams", async () => {
+        it("Should return all leagues of one team", async () => {
             expect(await service.searchLeagues("211")).to.deep.equal(
                 [{"id": 94,"name": "Primeira Liga","logo": "https://media.api-sports.io/football/leagues/94.png","country": "Portugal","seasons": [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]},{"id": 2,"name": "UEFA Champions League","logo": "https://media.api-sports.io/football/leagues/2.png","country": "World","seasons": [2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,2022,2023,2024]},{"id": 96,"name": "Taça de Portugal","logo": "https://media.api-sports.io/football/leagues/96.png","country": "Portugal","seasons": [2016,2017,2018,2019,2020,2021,2022,2023,2024]},{"id": 550,"name": "Super Cup","logo": "https://media.api-sports.io/football/leagues/550.png","country": "Portugal","seasons": [2016,2017,2019,2020,2023]},{"id": 18,"name": "AFC Cup","logo": "https://media.api-sports.io/football/leagues/18.png","country": "World","seasons": [2017,2018]},{"id": 26,"name": "International Champions Cup","logo": "https://media.api-sports.io/football/leagues/26.png","country": "World","seasons": [2018,2019]},{"id": 3,"name": "UEFA Europa League","logo": "https://media.api-sports.io/football/leagues/3.png","country": "World","seasons": [2018,2019,2020,2023]},{"id": 97,"name": "Taça da Liga","logo": "https://media.api-sports.io/football/leagues/97.png","country": "Portugal","seasons": [2019,2020,2021,2022,2023,2024]},{"id": 667,"name": "Friendlies Clubs","logo": "https://media.api-sports.io/football/leagues/667.png","country": "World","seasons": [2020,2021,2022,2023,2024]}]
             );
         });
 
-        it("Should return empty for non-existing team", async () => {
-            expect(await service.searchLeagues("osdvbhs")).to.deep.equal([]);
+        it("Should return empty for invalid team id", async () => {
+            expect(await service.searchLeagues("0")).to.deep.equal([]);
         });
-
     });
 
     describe("createGroup()", () => {
         it("Should return the created group", async () => {
-            const user = (await service.createUser("Joniel")).token;
-            expect(await service.createGroup("grupinho", "teste grupo", [], user)).to.deep.equal({
+            const user = (await service.createUser("Alice")).token;
+
+            expect(await service.createGroup("Grupo 1", "O meu primeiro grupo", [], user)).to.deep.equal({
                 "id": 1,
-                "name": "grupinho",
-                "description": "teste grupo",
+                "name": "Grupo 1",
+                "description": "O meu primeiro grupo",
                 "teams": []
             });
         });
 
-        it("Should return the created group (default vals)", async () => {
-            const user = (await service.createUser("Joniel")).token;
-            expect(await service.createGroup("grupinho", undefined, undefined, user)).to.deep.equal({
+        it("Should return the created group (using default values)", async () => {
+            const user = (await service.createUser("Alice")).token;
+
+            expect(await service.createGroup("Grupo 1", undefined, undefined, user)).to.deep.equal({
                 "id": 1,
-                "name": "grupinho",
+                "name": "Grupo 1",
                 "description": "",
                 "teams": []
             });
         });
 
         it("Should return the created group with team/s", async () => {
-            const user = (await service.createUser("Joniel")).token;
-            expect(await service.createGroup("Awesome Group 2", "This is the second best group ever!", [
+            const user = (await service.createUser("Alice")).token;
+
+            expect(await service.createGroup("Grupo 1", "O meu primeiro grupo", [
                 {
                     "id": 211,
                     "leagueId": 94,
                     "season": 2022
                 }
             ], user)).to.deep.equal(
-                {"id": 1,"name": "Awesome Group 2","description": "This is the second best group ever!","teams": [{"id": 211,"name": "Benfica","leagueId": 94,"league": "Primeira Liga","season": 2022,"stadium": "Estádio do Sport Lisboa e Benfica (da Luz)"}]}
+                {"id": 1,"name": "Grupo 1","description": "O meu primeiro grupo","teams": [{"id": 211,"name": "Benfica","leagueId": 94,"league": "Primeira Liga","season": 2022,"stadium": "Estádio do Sport Lisboa e Benfica (da Luz)"}]}
             );
 
-            expect(await service.createGroup("Awesome Group 2", "This is the second best group ever!", [
+            expect(await service.createGroup("Grupo 2", "O meu segundo grupo", [
                 {
                     "id": 211,
                     "leagueId": 94,
@@ -82,7 +84,7 @@ describe("Service", () => {
                     "season": 2022
                 }
             ], user)).to.deep.equal(
-                {"id": 2,"name": "Awesome Group 2","description": "This is the second best group ever!","teams": [{"id": 211,"name": "Benfica","leagueId": 94,"league": "Primeira Liga","season": 2022,"stadium": "Estádio do Sport Lisboa e Benfica (da Luz)"},{"id": 212,"name": "FC Porto","leagueId": 94,"league": "Primeira Liga","season": 2022,"stadium": "Estádio Do Dragão"}]}
+                {"id": 2,"name": "Grupo 2","description": "O meu segundo grupo","teams": [{"id": 211,"name": "Benfica","leagueId": 94,"league": "Primeira Liga","season": 2022,"stadium": "Estádio do Sport Lisboa e Benfica (da Luz)"},{"id": 212,"name": "FC Porto","leagueId": 94,"league": "Primeira Liga","season": 2022,"stadium": "Estádio Do Dragão"}]}
             );
         });
     });
@@ -92,65 +94,66 @@ describe("Service", () => {
     });
 
     describe("listGroups()", () => {
-        it("Should return all Groups", async () => {
-            const user = (await service.createUser("Joniel")).token;
+        it("Should return all user groups", async () => {
+            const user = (await service.createUser("Alice")).token;
             await service.createGroup("Grupo 1", "", [], user);
             await service.createGroup("Grupo 2", "", [], user);
+
             expect(await service.listGroups(user)).to.deep.equal([1, 2]);
         });
 
-        it("Should return empty for non-existing groups created", async () => {
-            const user = (await service.createUser("Joniel")).token;
+        it("Should return empty for users without groups", async () => {
+            const user = (await service.createUser("Alice")).token;
+
             expect(await service.listGroups(user)).to.deep.equal([]);
         });
         
-        it("Should return error s2", async () => {
+        it("Should return error s2 when no valid token is provided", async () => {
             await expect(service.listGroups("A1B2C3D4")).to.be.rejectedWith(Error).then(null, e => expect(e.actual).to.deep.include({ code: "s2" }));
         });
     });
     
     describe("deleteGroup()", () => {
-      it("Should return the confirmation of deletion of the group", async () => {
-        const user = (await service.createUser("Joniel")).token;
-        service.createGroup("Grupo teste", "", [], user)
-        service.createGroup("Grupo teste 2", "", [], user)
-        expect(await service.deleteGroup("1")).fulfilled
-      });
+        it("Should delete the user's group", async () => {
+            const user = (await service.createUser("Alice")).token;
+            await service.createGroup("Grupo 1", "", [], user);
 
-      it("Should return the confirmation of deletion of the group (with teams)", async () => {
-        const user = (await service.createUser("Joniel")).token;
-        service.createGroup("Grupo teste", "", [{"id": 211, "leagueId": 3, "season": 2019}], user)
-        service.createGroup("Grupo teste 2", "", [], user)
-        expect(await service.deleteGroup("1")).fulfilled
-      }); //TODO confirmar com teste api
+            expect(service.deleteGroup(1, user)).fulfilled;
+        });
 
-      it("Should return the error s3", async () => {
-        const user = (await service.createUser("Joniel")).token;
-        service.createGroup("Grupo teste", "", [], user)
-        service.createGroup("Grupo teste 2", "", [], user)
-        await expect(service.deleteGroup("7")).to.be.rejectedWith(Error).then(null, e => expect(e.actual).to.deep.include({ code: "s3" }));
-      });
+        it("Should return the confirmation of deletion of the group (with teams)", async () => {
+            const user = (await service.createUser("Alice")).token;
+            await service.createGroup("Grupo 1", "", [{"id": 211, "leagueId": 3, "season": 2019}], user);
+
+            expect(service.deleteGroup(1, user)).fulfilled;
+        });
+
+        it("Should return the error s3 when no such group exists", async () => {
+            const user = (await service.createUser("Alice")).token;
+
+            await expect(service.deleteGroup(7, user)).to.be.rejectedWith(Error).then(null, e => expect(e.actual).to.deep.include({ code: "s3" }));
+        });
     });
 
     describe("getGroupDetails()", () => {
-      it("Should return the group details", async () => {
-        const user = (await service.createUser("Joniel")).token;
-        service.createGroup("Grupo teste", "conteudo manhoso", [], user)
-        service.createGroup("Grupo teste 2", "", [], user)
-        expect(await service.getGroupDetails("1", user)).to.deep.equals({
-            "id": 1,
-            "name": "Grupo teste",
-            "description": "conteudo manhoso",
-            "teams": []
-        })
-      });
-      
-      it("Should return the error s3", async () => {
-        const user = (await service.createUser("Joniel")).token;
-        service.createGroup("Grupo teste", "conteudo manhoso", [], user)
-        service.createGroup("Grupo teste 2", "", [], user)
-        await expect(service.getGroupDetails("7", user)).to.be.rejectedWith(Error).then(null, e => expect(e.actual).to.deep.include({ code: "s3" }));
-      });
+        it("Should return the group details", async () => {
+            const user = (await service.createUser("Alice")).token;
+            await service.createGroup("Grupo 1", "O meu primeiro grupo", [], user);
+
+            expect(await service.getGroupDetails(1, user)).to.deep.equals({
+                "id": 1,
+                "name": "Grupo 1",
+                "description": "O meu primeiro grupo",
+                "teams": []
+            });
+        });
+        
+        it("Should return the error s3 when no such group exists", async () => {
+            const user = (await service.createUser("Alice")).token;
+            await service.createGroup("Grupo 1", "O meu primeiro grupo", [], user);
+
+            await expect(service.getGroupDetails(7, user)).to.be.rejectedWith(Error).then(null, e => expect(e.actual).to.deep.include({ code: "s3" }));
+        });
     });
 
     describe("addTeamsToGroup()", () => {
@@ -162,8 +165,8 @@ describe("Service", () => {
     });
 
     describe("createUser()", () => {
-        it("Should return all Groups", async () => {
-            expect(await service.createUser("Joniel")).to.have.all.keys("id", "name", "token");
+        it("Should create a new user and return its details", async () => {
+            expect(await service.createUser("Alice")).to.have.all.keys("id", "name", "token");
         });
     }); 
 });
